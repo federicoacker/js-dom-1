04f393d2ae6686a445e6305ada65ba4fa041c8e4
federicoacker <acker.federico@gmail.com>
2026-04-03 10:04:43 +0200

    Piccole modifiche alla logica di controllo

--- scripts/index.js
+++ scripts/index.js
@@ -5,13 +5,15 @@ const containerElement = document.querySelector("#contenitore-lampadina > div");
 
 ///////// FUNCTIONS /////////
 function clickBtnElementHandler(){ //Click Handler
-    const btnElementHTML = btnElement.innerHTML; // vediamo l'innerHTML del bottone
-    if(btnElementHTML.toLowerCase().includes("spegni")){ // Se include "spegni" allora vogliamo spegnere la lampadina
+    const imageElementSrc = imageElement.src; // vediamo la src dell'immagine
+    if(imageElementSrc.toLowerCase().includes("yellow")){ // Se include "yellow" allora vogliamo spegnere la lampadina
         imageElement.src = "./img/white_lamp.png"; // Settiamo la src dell'immagine alla lampadina spenta
+        imageElement.alt = "Immagine di una lampadina spenta";
         btnElement.innerHTML = "Accendi la Lampadina"; // Cambiamo la scritta del bottone in modo che dica Accendi la Lampadina
     }
-    else if(btnElementHTML.toLowerCase().includes("accendi")){// Altrimenti se include "accendi" vogliamo accendere la lampadina
+    else if(imageElementSrc.toLowerCase().includes("white")){// Altrimenti se include "white" vogliamo accendere la lampadina
         imageElement.src = "./img/yellow_lamp.png"; // Settiamo la src dell'immagine alla lampadina accesa
+        imageElement.alt = "Immagine di una lampadina accesa";
         btnElement.innerHTML = "Spegni la Lampadina"; // Cambiamo la scritta del bottone in modo che dica Spegni la Lampadina
     }
     containerElement.classList.toggle("bg-black"); // Toggle della classe bg-black nel div superiore in modo da simulare la stanza che si illumina
